8aeb32d5a225409c18bdb833a948795d85c5798e
theyak <yakkyjunk@hotmail.com>
2016-06-16 17:45:08 -0700

    Add static functions to TauEncryption

--- modules/TauEncryption.php
+++ modules/TauEncryption.php
@@ -10,6 +10,39 @@
  *
  */
 
+/*
+Examples:
+
+// Static calls
+$encoded = TauEncryption::encrypt( 'bob', 'hi' );
+Tau::dump($encoded);
+$decoded = TauEncryption::decrypt( $encoded, 'hi' );
+Tau::dump($decoded);
+
+// Instance, passing in key
+$e = new TauEncryption( 'hi' );
+$encoded = $e->encode( 'bob' );
+Tau::dump($encoded);
+$decoded = $e->decode( $encoded );
+Tau::dump($decoded);
+
+// Instance, using key directly in parameters
+$e = new TauEncryption();
+$encoded = $e->encode( 'bob', 'hi' );
+Tau::dump($encoded);
+$decoded = $e->decode( $encoded, 'hi' );
+Tau::dump($decoded);
+
+// Instance, assign key separately
+$e = new TauEncryption();
+$e->key = 'hi';
+$encoded = $e->encode( 'bob' );
+Tau::dump($encoded);
+$decoded = $e->decode( $encoded );
+Tau::dump($decoded);
+*/
+
+
 if (!defined('TAU'))
 {
 	exit;
@@ -23,14 +56,23 @@ class TauEncryption
 	 */
 	public $key = '';
 
-	private function safe_b64encode($string)
+
+	public function __construct( $key = null )
+	{
+		if ( ! is_null( $key ) )
+		{
+			$this->key = $key;
+		}
+	}
+
+	private static function safe_b64encode($string)
 	{
 		$data = base64_encode($string);
 		$data = str_replace(array('+','/','='), array('-','_',''), $data);
 		return $data;
 	}
 
-	private function safe_b64decode($string)
+	private static function safe_b64decode($string)
 	{
 		$data = str_replace(array('-','_'), array('+','/'), $string);
 		$mod4 = strlen($data) % 4;
@@ -50,12 +92,11 @@ class TauEncryption
 			$key = $this->key;
 		}
 
-		$text = $value;
 		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
 		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-		$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_ECB, $iv);
+		$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $value, MCRYPT_MODE_ECB, $iv);
 
-		return trim($this->safe_b64encode($crypttext));
+		return trim(static::safe_b64encode($crypttext));
 	}
 
 	public function decode($value, $key = '')
@@ -67,11 +108,40 @@ class TauEncryption
 			$key = $this->key;
 		}
 
-		$crypttext = $this->safe_b64decode($value);
+		$crypttext = static::safe_b64decode($value);
 		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
 		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 		$decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $crypttext, MCRYPT_MODE_ECB, $iv);
 
 		return trim($decrypttext);
+	}
+
+
+	public static function encrypt( $value, $key )
+	{
+		if ( ! is_string( $value ) || strlen( $value ) <= 0 ) {
+			return false;
+		}
+
+		if ( ! is_string( $key ) || strlen( $key ) <= 0 ) {
+			return false;
+		}
+
+		$iv_size = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
+		$iv = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
+		$crypttext = mcrypt_encrypt( MCRYPT_RIJNDAEL_256, $key, $value, MCRYPT_MODE_ECB, $iv );
+
+		return static::safe_b64encode( $crypttext );
+	}
+
+
+	public static function decrypt( $value, $key )
+	{
+		$crypttext = static::safe_b64decode( $value );
+		$iv_size = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
+		$iv = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
+		$decrypttext = mcrypt_decrypt( MCRYPT_RIJNDAEL_256, $key, $crypttext, MCRYPT_MODE_ECB, $iv );
+
+		return $decrypttext;
 	}
 }
