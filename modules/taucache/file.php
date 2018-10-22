2f92f6d7f0ce2903878d2757d673c3991f23c75a
theyak <yakkyjunk@hotmail.com>
2018-10-22 11:30:15 -0700

    Disable E_NOTICE or E_WARNING when we don't care about them.

--- modules/taucache/file.php
+++ modules/taucache/file.php
@@ -25,8 +25,8 @@ class TauCacheFile
 	 * Path of cache data files
 	 */
 	private $path;
-	
-	
+
+
 	/**
 	 * Previously loaded cache data
 	 * @var array()
@@ -92,7 +92,7 @@ class TauCacheFile
 			'1.0',
 			$ttl ? time() + $ttl : 0,
 		);
-		
+
 		if ( isset( $opts[ 'notes' ] ) )
 		{
 			$lines[] = trim( preg_replace('/[\n\r\s\t]+/', ' ', $opts[ 'notes' ] ) );
@@ -101,10 +101,10 @@ class TauCacheFile
 		{
 			$lines[] = '';
 		}
-		
+
 		$lines[] = strlen( $data );
 		$lines[] = $data;
-		
+
 		file_put_contents( $file, implode( "\n", $lines ) );
 	}
 
@@ -123,11 +123,12 @@ class TauCacheFile
 		{
 			return static::$cache_results[ $key ][ 'data' ];
 		}
-		
+
 		$file = $this->path . $key . '.php';
 		if ( is_file( $file ) )
 		{
-			$f = fopen( $file, 'rb' );
+			// This sometimes fails, how?!
+			$f = @fopen( $file, 'rb' );
 			if ( $f )
 			{
 				// Read header line
@@ -145,7 +146,7 @@ class TauCacheFile
 				if ( $expires >= time() || $expires <= 0 )
 				{
 					$notes = fgets( $f );
-					if ( preg_match( '/^[0-9]+$/', $notes ) ) 
+					if ( preg_match( '/^[0-9]+$/', $notes ) )
 					{
 						$length = (int) $notes;
 					}
@@ -155,24 +156,27 @@ class TauCacheFile
 					}
 					$data = fread( $f, $length );
 					fclose( $f );
-					if ( strlen( $data ) === $length );
+					if ( strlen( $data ) === $length )
 					{
-						$data = unserialize( $data );
-						static::$cache_results[ $key ] = array(
-							'data' => $data,
-							'notes' => $notes,
-						);
-							
-						return $data;
+						// Disable E_NOTICE for bad data
+						$old = error_reporting(E_ALL & ~E_NOTICE);
+						$data = unserialize($data);
+						error_reporting($old);
+						if ($data) {
+							static::$cache_results[$key] = [
+								'data' => $data,
+								'notes' => $notes,
+							];
+
+							return $data;
+						}
 					}
+					$this->remove($file);
 				}
 				else
 				{
-					fclose( $f );
-					if ( is_file( $file ) )
-					{
-						@unlink( $file );
-					}
+					fclose($f);
+					$this->remove($file);
 				}
 			}
 		}
@@ -180,10 +184,10 @@ class TauCacheFile
 	}
 
 
-	
+
 	/**
 	 * Get note for a cache entry
-	 * 
+	 *
 	 * @param string $key
 	 * @return string
 	 */
@@ -193,16 +197,16 @@ class TauCacheFile
 		{
 			$result = $this->get( $key );
 		}
-		
+
 		if ( isset( static::$cache_results[ $key ] ) )
 		{
 			return static::$cache_results[ $key ][ 'notes' ];
 		}
-		
+
 		return '';
 	}
-	
-	
+
+
 
 	/**
 	 * Check of a key exists in cache
@@ -234,7 +238,7 @@ class TauCacheFile
 				}
 				else
 				{
-					@unlink( $file );
+					$this->remove($file);
 				}
 			}
 		}
@@ -253,7 +257,9 @@ class TauCacheFile
 	{
 		if ( is_file( $this->path . $key . '.php' ) )
 		{
-			unlink( $this->path . $key . '.php' );
+			$old = error_reporting(E_ALL & ~E_WARNING);
+			unlink($this->path . $key . '.php');
+			error_reporting($old);
 		}
 	}
 
@@ -276,33 +282,34 @@ class TauCacheFile
 			$this->set( $key, $value );
 			return $value;
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 
 	/**
 	 * Retrieve all, or subset thereof, the keys of object in cache
-	 * 
+	 *
 	 * @param string|boolean $prefix Prefix for keys to return
 	 * @return string[]
 	 */
 	function keys( $prefix = false )
 	{
 		$length = strlen( $this->path );
+
 		$files = TauFS::rdir( $this->path );
-		
+
 		$files = array_map( function ( $value ) use( $length ) {
 			return substr( $value, $length );
 		}, $files );
-		
-		
+
+
 		// Only get files of specified prefix - should change to regex
 		if ( is_string( $prefix ) )
 		{
 			$length = strlen( $prefix );
-			foreach ( $files AS $key => $value ) 
+			foreach ( $files AS $key => $value )
 			{
 				if ( substr( $value, 0, $length ) !== $prefix )
 				{
@@ -310,8 +317,8 @@ class TauCacheFile
 				}
 			}
 		}
-		
-		
+
+
 		// Make sure it's a valid cache file (sorta)
 		foreach ( $files AS $key => $value )
 		{
@@ -324,7 +331,7 @@ class TauCacheFile
 				$files[ $key ] = substr( $value, 0, strlen( $value ) - 4 );
 			}
 		}
-		
+
 		return $files;
-	}	
+	}
 }
